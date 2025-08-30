e639148affee9e84046a88340264777b0133316d
Algo2147483647 <nullllllllun@gmail.com>
2025-08-30 22:08:12 +0800

    four-order equation

--- src-golang/model/shape/forth-order_equation_test.go
+++ src-golang/model/shape/forth-order_equation_test.go
@@ -1,36 +1,65 @@
 package shape
 
 import (
+	"fmt"
 	"src-golang/math_lib"
 	"testing"
 )
 
 func TestFourOrderEquation(t *testing.T) {
-	a := math_lib.NewTensor[float64]([]int{4, 4, 4, 4})
+	t.Run("(x - 1)^4 + y^4 + z^4 = 1", func(t *testing.T) {
+		a := math_lib.NewTensor[float64]([]int{4, 4, 4, 4})
 
-	a.Set(1, 0, 0, 0, 0)
-	a.Set(-1, 1, 1, 1, 1)
-	a.Set(-1, 2, 2, 2, 2)
-	a.Set(-1, 3, 3, 3, 3)
+		a.Set(1, 0, 0, 0, 0)
+		a.Set(-1, 1, 1, 1, 1)
+		a.Set(-1, 2, 2, 2, 2)
+		a.Set(-1, 3, 3, 3, 3)
 
-	a.Set(a.Get(1, 1, 1, 1)-1, 1, 1, 1, 1)
-	a.Set(a.Get(1, 1, 1, 0)+4, 1, 1, 1, 0)
-	a.Set(a.Get(1, 1, 0, 0)-6, 1, 1, 0, 0)
-	a.Set(a.Get(1, 0, 0, 0)+4, 1, 0, 0, 0)
-	a.Set(a.Get(0, 0, 0, 0)-1, 0, 0, 0, 0)
+		a.Set(a.Get(1, 1, 1, 1)-1, 1, 1, 1, 1)
+		a.Set(a.Get(1, 1, 1, 0)+4, 1, 1, 1, 0)
+		a.Set(a.Get(1, 1, 0, 0)-6, 1, 1, 0, 0)
+		a.Set(a.Get(1, 0, 0, 0)+4, 1, 0, 0, 0)
+		a.Set(a.Get(0, 0, 0, 0)-1, 0, 0, 0, 0)
 
-	for i, v := range a.Data {
-		if i%64 == 0 {
-			println()
-			println()
-		} else if i%16 == 0 {
-			println()
-		} else if i%4 == 0 {
-			print("   ")
+		for i, v := range a.Data {
+			if i%64 == 0 {
+				println()
+				println()
+			} else if i%16 == 0 {
+				println()
+			} else if i%4 == 0 {
+				print("   ")
+			}
+
+			print(int(v))
+			print(", ")
 		}
+		println()
+	})
 
-		print(int(v))
-		print(", ")
-	}
-	println()
+	t.Run("Tanglecube: sum_{i=1}^3 (x_i^4 - 5 x_i^2 ) + 11.8 = 0, (x - 1.5), ", func(t *testing.T) {
+		a := math_lib.NewTensor[float64]([]int{4, 4, 4, 4})
+
+		a.Set(11.8, 0, 0, 0, 0)
+		a.Set(1, 1, 1, 1, 1)
+		a.Set(1, 2, 2, 2, 2)
+		a.Set(1, 3, 3, 3, 3)
+		a.Set(-5, 1, 1, 0, 0)
+		a.Set(-5, 2, 2, 0, 0)
+		a.Set(-5, 3, 3, 0, 0)
+
+		for i, v := range a.Data {
+			if i%64 == 0 {
+				println()
+				println()
+			} else if i%16 == 0 {
+				println()
+			} else if i%4 == 0 {
+				print("   ")
+			}
+
+			fmt.Printf("%.2f,", v)
+		}
+		println()
+	})
 }
