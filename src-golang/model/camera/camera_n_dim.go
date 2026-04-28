fe37bd04ce0c331eb5319fd452b2143ce50c4358
Algo2147483647 <nullllllllun@gmail.com>
2026-04-29 00:29:31 +0800

    ai

--- src-golang/model/camera/camera_n_dim.go
+++ src-golang/model/camera/camera_n_dim.go
@@ -1,6 +1,7 @@
 package camera
 
 import (
+	"fmt"
 	math_lib "github.com/Algo2147483647/golang_toolkit/math/linear_algebra"
 	"gonum.org/v1/gonum/mat"
 	"math"
@@ -10,16 +11,49 @@ import (
 
 type CameraNDim struct {
 	CameraBase
-	Position    *mat.VecDense   // 相机位置
-	Coordinates []*mat.VecDense // 坐标系
-	Width       []int           // 像素宽度
-	FieldOfView []float64       // 视野角度 (度)
-	Ortho       bool            // 正交相机 / 透视相机
+	Position    *mat.VecDense
+	Coordinates []*mat.VecDense
+	Width       []int
+	FieldOfView []float64
+	Ortho       bool
+
+	orthonormalCoordinates []*mat.VecDense
+	fovTangents            []float64
 }
 
-// NewCamera 创建新相机
 func NewCameraNDim() *CameraNDim {
 	return &CameraNDim{}
+}
+
+func (c *CameraNDim) Prepare() error {
+	if len(c.Coordinates) == 0 {
+		return fmt.Errorf("camera coordinates are not configured")
+	}
+	if len(c.Width) == 0 {
+		return fmt.Errorf("camera width is not configured")
+	}
+	if len(c.Width) != len(c.FieldOfView) {
+		return fmt.Errorf("width count %d does not match field of view count %d", len(c.Width), len(c.FieldOfView))
+	}
+	if len(c.Coordinates) != len(c.Width)+1 {
+		return fmt.Errorf("coordinate count %d must equal width count + 1 (%d)", len(c.Coordinates), len(c.Width)+1)
+	}
+
+	c.orthonormalCoordinates = math_lib.GramSchmidt(c.Coordinates...)
+	c.fovTangents = make([]float64, len(c.FieldOfView))
+	for i, fov := range c.FieldOfView {
+		c.fovTangents[i] = math.Tan(fov * math.Pi / 180 / 2.0)
+	}
+
+	return nil
+}
+
+func (c *CameraNDim) ensurePrepared() error {
+	if len(c.orthonormalCoordinates) == len(c.Coordinates) && len(c.fovTangents) == len(c.FieldOfView) {
+		return nil
+	}
+
+	return c.Prepare()
 }
 
 func (c *CameraNDim) GenerateRay(res *optics.Ray, x ...int) *optics.Ray {
@@ -28,20 +62,20 @@ func (c *CameraNDim) GenerateRay(res *optics.Ray, x ...int) *optics.Ray {
 	}
 	res.Init()
 
-	var (
-		u           = make([]float64, len(x))
-		coordinates = math_lib.GramSchmidt(c.Coordinates...) // 正交化所有向量，确保它们互相垂直
-	)
+	if err := c.ensurePrepared(); err != nil {
+		panic(err)
+	}
 
+	u := make([]float64, len(x))
 	for i := 0; i < len(x); i++ {
 		u[i] = 2*(float64(x[i])+rand.Float64())/float64(c.Width[i]) - 1
 	}
 
 	res.Color = mat.NewVecDense(3, []float64{1, 1, 1})
 	res.Origin.CloneFromVec(c.Position)
-	res.Direction.CloneFromVec(coordinates[0])
+	res.Direction.CloneFromVec(c.orthonormalCoordinates[0])
 	for i := 0; i < len(x); i++ {
-		res.Direction.AddScaledVec(res.Direction, u[i]*math.Tan(c.FieldOfView[i]*math.Pi/180/2.0), coordinates[i+1])
+		res.Direction.AddScaledVec(res.Direction, u[i]*c.fovTangents[i], c.orthonormalCoordinates[i+1])
 	}
 	math_lib.Normalize(res.Direction)
 
